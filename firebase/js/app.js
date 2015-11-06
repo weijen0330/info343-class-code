ef010da4c640744834ef56bf066f42e4d2e3d7f3
Dave Stearns <dlsinfo@uw.edu>
2015-11-05 20:25:30 -0800

    added auth with github; adjusted display to use profile images

--- firebase/js/app.js
+++ firebase/js/app.js
@@ -1,28 +1,41 @@
 
 angular.module('ChatApp', ['firebase'])
-    .constant('firebaseUrl', 'https://info343chat.firebaseio.com/messages')
-    .controller('ChatController', function($scope, $firebaseArray, firebaseUrl) {
+    .constant('firebaseUrl', 'https://info343chat.firebaseio.com')
+    .controller('ChatController', function($scope, $firebaseArray, $firebaseObject, $firebaseAuth, firebaseUrl) {
         //create reference to the Firebase
-        var ref = new Firebase(firebaseUrl);
-        ref.limitToLast(1000);
+        var rootRef = new Firebase(firebaseUrl);
+        var auth = $firebaseAuth(rootRef);
+        $scope.users = $firebaseObject(rootRef.child('users'));
 
-        $scope.messages = $firebaseArray(ref);
+        auth.$onAuth(function(authData) {
+            if (authData) {
+                $scope.users[authData.github.username] = authData.github;
+                $scope.users.$save();
 
-        //initialize form fields
-        $scope.name = null;
-        $scope.body = null;
+                $scope.user = authData.github;
+
+                var messagesRef = rootRef.child('messages');
+                messagesRef.limitToLast(1000);
+                $scope.messages = $firebaseArray(messagesRef);
+            }
+            else {
+                auth.$authWithOAuthRedirect('github')
+                    .catch(function(err) {
+                        console.error(err);
+                    });
+            }
+        });
 
         $scope.sendMessage = function() {
             //adds a new object to the array and synchronizes
             //with the server
             $scope.messages.$add({
-                name: $scope.name,
+                username: $scope.user.username,
                 body: $scope.body,
                 createdAt: Firebase.ServerValue.TIMESTAMP
             });
 
             $scope.body = null;
         }; //sendMessage()
-
 
     });
