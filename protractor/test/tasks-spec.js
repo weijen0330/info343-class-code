5f6672f80dadfb4aa3d61edc01b46d1c5a0a3f66
Dave Stearns <dlsinfo@uw.edu>
2015-11-13 16:28:34 -0800

    add lots of comments

--- protractor/test/tasks-spec.js
+++ protractor/test/tasks-spec.js
@@ -1,12 +1,32 @@
 /* Test script for the Tasks List app */
+
+//describe() creates a new set of test cases
+//you can also include a describe() inside another
+//describe if you want to create a subset of related tests
 describe('the tasks app', function() {
+    //this callback function is just a normal JavaScript
+    //function, so you can create variables and custom
+    //functions to make your tests easier to write
+
+    //create some variables that reference elements in the page
+    //element() is used to create an element reference
+    //element() takes a 'locator' and you can create locators
+    //using the `by` object
+    //see https://angular.github.io/protractor/#/api?view=ProtractorBy
+    //and https://angular.github.io/protractor/#/api?view=ElementFinder.prototype.$
+    //for details
     var taskTitleInp = element(by.model('newTask.title'));
     var addTaskBtn = element(by.buttonText('Add Task'));
+    //element.all() gets a reference to set of elements
     var tasksList = element.all(by.repeater('task in tasks'));
     var requiredMsg = $('.title-required-error');
 
     function addTask(title) {
+        //sendKeys literally types text into an input
         taskTitleInp.sendKeys(title);
+        
+        //the .click() method clicks the button 
+        //just like a user would
         addTaskBtn.click();
     }
 
@@ -17,11 +37,21 @@ describe('the tasks app', function() {
         }
     }
 
+    //the function passed to beforeEach()
+    //will execute before each test
     beforeEach(function() {
+        //reload the page so we start the test fresh
         browser.get('http://localhost:8000/');
     });
 
+    //each test is defined using the it() function
+    //the callback function passed as the second param
+    //creates an execution context for the test
     it('must have the proper page title', function() {
+        //the expect() and related functions are defined
+        //by the Jasmine library
+        //see http://jasmine.github.io/2.3/introduction.html
+        //for details
         expect(browser.getTitle()).toEqual('My Tasks');
     });
 
@@ -42,6 +72,9 @@ describe('the tasks app', function() {
 
     it('must clear the title after adding', function() {
         addTask('box should get cleared');
+        //this is how you get the current value in an input control
+        //use getAttribute('value')
+        //don't try getText() because it won't work as expected
         expect(taskTitleInp.getAttribute('value')).toEqual('');
     });
 
@@ -52,8 +85,11 @@ describe('the tasks app', function() {
     });
 
     it('must show required validation error', function() {
+        //use isPresent() to test if an element that has an
+        //ng-if is in the DOM or not
         expect(requiredMsg.isPresent()).toEqual(false);
         taskTitleInp.sendKeys('abc');
+        //use .clear() to clear an input
         taskTitleInp.clear();
         expect(requiredMsg.isPresent()).toEqual(true);
         taskTitleInp.sendKeys('abc');
@@ -61,6 +97,10 @@ describe('the tasks app', function() {
     });
 
     it('must disable add task button with blank title', function() {
+        //use getAttribute('disabled') to test whether a button
+        //is currently disabled
+        //note that the value will be 'true' (as a string) if the
+        //button is disabled, or null if the button is enabled
         expect(addTaskBtn.getAttribute('disabled')).toEqual('true');
         taskTitleInp.sendKeys('abc');
         expect(addTaskBtn.getAttribute('disabled')).toBe(null);
@@ -74,6 +114,9 @@ describe('the tasks app', function() {
         addTask('not marked as done');
         expect(tasksList.count()).toEqual(2);
         tasksList.get(0).click();
+        //when testing for style classes, use toContain()
+        //instead of toEqual(), as there may be multiple
+        //style classes added to the element
         expect(tasksList.get(0).getAttribute('class'))
             .toContain('completed-task');
         expect(tasksList.get(1).getAttribute('class'))
